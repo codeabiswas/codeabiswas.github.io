a319fd0376930e78f1a5557eef2d9cb275feb6d8
Andrei Biswas <axb6972@rit.edu>
2019-05-27 12:52:42 -0400

    Working on a new, much nicer looking design

---
+++ new_design/index.js
@@ -0,0 +1,72 @@
+function showabout(){
+    $("#about_container").css("display","inherit");
+    $("#about_container").addClass("animated slideInLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideInLeft");
+    },800);
+}
+function closeabout(){
+    $("#about_container").addClass("animated slideOutLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideOutLeft");
+        $("#about_container").css("display","none");
+    },800);
+}
+function showwork(){
+    $("#work_container").css("display","inherit");
+    $("#work_container").addClass("animated slideInRight");
+    setTimeout(function(){
+        $("#work_container").removeClass("animated slideInRight");
+    },800);
+}
+function closework(){
+    $("#work_container").addClass("animated slideOutRight");
+    setTimeout(function(){
+        $("#work_container").removeClass("animated slideOutRight");
+        $("#work_container").css("display","none");
+    },800);
+}
+function showcontact(){
+    $("#contact_container").css("display","inherit");
+    $("#contact_container").addClass("animated slideInUp");
+    setTimeout(function(){
+        $("#contact_container").removeClass("animated slideInUp");
+    },800);
+}
+function closecontact(){
+    $("#contact_container").addClass("animated slideOutDown");
+    setTimeout(function(){
+        $("#contact_container").removeClass("animated slideOutDown");
+        $("#contact_container").css("display","none");
+    },800);
+}
+function changemode(){
+    
+    // Get the body's attributes
+    var body = document.getElementById("body");
+    
+    // Check for the background color and swap accordingly
+    // If the background color is black
+    if( body.style.backgroundColor == "black" ) {
+        // Change it to white
+        body.style.backgroundColor = "white";
+        // Change the switch button's text
+        document.getElementById("btnswitchmode").innerHTML = "Dark Mode";
+    }
+    else{
+        body.style.backgroundColor = "black";
+        document.getElementById("btnswitchmode").innerHTML = "Light Mode";
+    }
+
+}
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+      $("#box").css("display","none");
+      $("#about").removeClass("animated fadeIn");
+      $("#contact").removeClass("animated fadeIn");
+      $("#work").removeClass("animated fadeIn");
+    },1000);
+},1500);
