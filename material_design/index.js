25e96191e0716bba1852711d4a2757ec3b0f28c2
Andrei Biswas <axb6972@rit.edu>
2019-06-15 00:22:49 -0400

    Many changes to the code. There is more structure to the computer view. Need to add mobile friendliness still.

--- material_design/index.js
+++ material_design/index.js
@@ -1,9 +1,54 @@
 setTimeout(function(){
     $("#loading").addClass("uk-animation-fade uk-animation-reverse");
-    $("#mainmenu").addClass("uk-animation-slide-top-small");
+    $("#header").addClass("uk-animation-slide-top-small");
+    $("#mainmenu").addClass("uk-animation-fade");
     $("#contacts").addClass("uk-animation-slide-bottom-small");
     setTimeout(function(){
         $("#loading").removeClass("uk-animation-fade uk-animation-reverse");
         $("#loading").css("display", "none");
     },1200);
 },1000);
+
+$("#tab-programming").click(function(){
+    document.getElementById("toggle-card").innerHTML = "Programming Clicked!";
+});
+$("#tab-software").click(function(){
+    document.getElementById("toggle-card").innerHTML = "Software Clicked!";
+});
+$("#tab-hardware").click(function(){
+    document.getElementById("toggle-card").innerHTML = "Hardware Clicked!";
+});
+$("#tab-languages").click(function(){
+    document.getElementById("toggle-card").innerHTML = "Languages Clicked!";
+});
+
+$("#tab-python").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "Python Projects";
+});
+$("#tab-shell").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "Shell Projects";
+});
+$("#tab-html5").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "HTML5 Projects";
+});
+$("#tab-arm-assembly").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "ARM Assembly Projects";
+});
+$("#tab-vhdl").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "VHDL Projects";
+});
+$("#tab-css3").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "CSS3 Projects";
+});
+$("#tab-javascript").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "JavaScript Projects";
+});
+$("#tab-java").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "Java Projects";
+});
+$("#tab-kotlin").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "Kotlin Projects";
+});
+$("#tab-c").click(function(){
+    document.getElementById("toggle-card-projects").innerHTML = "C Projects";
+});
